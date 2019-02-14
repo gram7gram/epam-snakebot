18a5110a3e38e7b481d86bc4b2295d6d030ae2b6
Gram <gram7gram@gmail.com>
2019-02-14 19:24:58 +0200

    #master fixed

--- constants.js
+++ constants.js
@@ -140,13 +140,6 @@ export const COMMANDS = {
     DIE: 'ACT(0)',
 };
 
-export const SECTORS = {
-    TOP_LEFT: 'TOP_LEFT',
-    TOP_RIGHT: 'TOP_RIGHT',
-    BOTTOM_LEFT: 'BOTTOM_LEFT',
-    BOTTOM_RIGHT: 'BOTTOM_RIGHT',
-};
-
 export const MAP = {
     WALKABLE: 0,
     BLOCKED: 1,
